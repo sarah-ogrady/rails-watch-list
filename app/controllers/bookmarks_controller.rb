b293a40c4c229e39688c20f081db295e3af4b4e7
sarah ogrady <sarahfrancesogrady@gmail.com>
2021-05-27 11:45:31 +0100

    finished the core app no css

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,24 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_bookmarks_path(@bookmark)
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
